0d9baa6877b2e071b94c0d171d246fed3e2e3b32
Alessandra Cannatà <alessandra.cannata@welaika.com>
2020-11-02 12:27:31 +0100

    Update code to rails 5 syntax

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_filter :authenticate_user!
-  before_filter :correct_user?
+  before_action :authenticate_user!
+  before_action :correct_user?
 
   def show
     @user = User.find(params[:id])
