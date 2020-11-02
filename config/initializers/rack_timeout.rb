b13ba70a88652a40874d8c548ac14e67776500e9
Alessandra Cannatà <alessandra.cannata@welaika.com>
2020-11-02 12:17:57 +0100

    Update rack-timeout initializer

--- config/initializers/rack_timeout.rb
+++ config/initializers/rack_timeout.rb
@@ -1,1 +1,1 @@
-# Rack::Timeout.timeout = (ENV["TIMEOUT_IN_SECONDS"] || 5).to_i
+Rails.application.config.middleware.insert_before Rack::Runtime, Rack::Timeout, service_timeout: (ENV["TIMEOUT_IN_SECONDS"] || 5).to_i
